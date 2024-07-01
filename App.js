549c5714544f1a79d773da23925d347a217c56e0
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-01 12:54:14 +0000

    Move app styles to separate file

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
+import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// Styles
+import { appStyles } from './styles/appStyles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style = {appStyles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

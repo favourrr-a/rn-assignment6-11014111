6e49729e361da60e1e3731cb1937d4e7dfd6611c
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-02 22:36:21 +0000

    Create components for cart items and list of cart items
    - CartItem represents each product that has been added to the cart
    - CartItemsList displays a list of all products that have been added to the cart as cart item cards

---
+++ components/cart-screen/CartItem.js
@@ -0,0 +1,25 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+// Styles
+import { cartItemStyles } from '../../styles/cart-screen/cartItemStyles';
+
+// Assets
+import remove from '../../assets/icons/remove-icon.png';
+
+export default function CartItem({product}) {
+    return(
+        <View style = {cartItemStyles.container}>
+            <Image source = {product.image} style = {cartItemStyles.image} resizeMode = 'contain'/>
+            <View style = {cartItemStyles.productDetailsAndRemoveContainer}>
+                <View style = {cartItemStyles.productDetailsContainer}>
+                    <Text style = {cartItemStyles.productCategory}>{product.category}</Text>
+                    <Text style = {cartItemStyles.productName}>{product.name}</Text>
+                    <Text style = {cartItemStyles.productPrice}>${product.price}</Text>
+                </View>
+                <TouchableOpacity style = {cartItemStyles.removeButtonContainer}>
+                    <Image source = {remove} style = {cartItemStyles.removeButton}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ styles/cart-screen/cartItemsListStyles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export const cartItemsListStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10,
+    },
+});

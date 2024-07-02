8aa74c8261140c1547ae77d4220af01a6f1ad7e0
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-02 21:33:08 +0000

    Add CheckoutHeader to cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -2,6 +2,7 @@ import { View } from 'react-native';
 
 // Custom components
 import CartScreenHeader from '../components/cart-screen/CartScreenHeader';
+import CheckoutHeader from '../components/cart-screen/CheckoutHeader';
 
 // Styles
 import { appStyles } from '../styles/appStyles';
@@ -12,6 +13,7 @@ export default function CartScreen() {
         <View style = {appStyles.container}>
             <View style = {cartScreenStyles.container}>
                 <CartScreenHeader/>
+                <CheckoutHeader/>
             </View>
         </View>
     )

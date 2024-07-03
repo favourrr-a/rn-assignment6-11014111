95b098b596d79cfbf614fd8cd379359da59b5551
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-03 19:29:38 +0000

    Add more products to list of products
    - I used different images for each product
    - names, categories, and prices of each product are dynamic too
    - there are a total of twenty products listed

--- components/home-screen/ProductsList.js
+++ components/home-screen/ProductsList.js
@@ -7,7 +7,27 @@ import ProductCard from './ProductCard';
 import { productsListStyles } from '../../styles/home-screen/productsListStyles';
 
 // Assets
-import productImage from '../../assets/products-images/dress1.jpg';
+import productImage1 from '../../assets/products-images/dress1.jpg';
+import productImage2 from '../../assets/products-images/dress2.jpg';
+import productImage3 from '../../assets/products-images/dress3.jpg';
+import productImage4 from '../../assets/products-images/dress4.jpg';
+import productImage5 from '../../assets/products-images/dress5.jpg';
+import productImage6 from '../../assets/products-images/dress6.jpg';
+import productImage7 from '../../assets/products-images/dress7.jpg';
+import productImage8 from '../../assets/products-images/dress8.jpg';
+import productImage9 from '../../assets/products-images/dress9.jpg';
+import productImage10 from '../../assets/products-images/dress10.jpg';
+import productImage11 from '../../assets/products-images/dress11.jpg';
+import productImage12 from '../../assets/products-images/dress12.jpg';
+import productImage13 from '../../assets/products-images/dress13.jpg';
+import productImage14 from '../../assets/products-images/dress14.jpg';
+import productImage15 from '../../assets/products-images/dress15.jpg';
+import productImage16 from '../../assets/products-images/dress16.jpg';
+import productImage17 from '../../assets/products-images/dress17.jpg';
+import productImage18 from '../../assets/products-images/dress18.jpg';
+import productImage19 from '../../assets/products-images/dress19.jpg';
+import productImage20 from '../../assets/products-images/dress20.jpg';
+
 
 export default function ProductsList(){
     const numColumns = 2;
@@ -15,74 +35,145 @@ export default function ProductsList(){
     const products = [
         {
             id: 1,
-            image: productImage,
-            category: 'Church wear',       
-            name: 'Product 1',
-            price: 100
+            image: productImage1,
+            category: 'Professional wear',       
+            name: 'executive elegant dress',
+            price: 120
         },
         {
             id: 2,
-            image: productImage,
-            category: 'Church wear',
-            name: 'Product 2',
-            price: 200
+            image: productImage2,
+            category: 'Casual wear',
+            name: 'floral off shoulder sundress',
+            price: 80
         },
         {
             id: 3,
-            image: productImage,
+            image: productImage3,
             category: 'Church wear',
-            name: 'Product 3',
-            price: 300
+            name: 'minimalist dress',
+            price: 115
         },
         {
             id: 4,
-            image: productImage,
-            category: 'Church wear',
-            name: 'Product 4',
-            price: 400
+            image: productImage4,
+            category: 'Formal wear',
+            name: 'glamorous black dress',
+            price: 130
         },
         {
             id: 5,
-            image: productImage,
+            image: productImage5,
             category: 'Church wear',
-            name: 'Product 5',
-            price: 500
+            name: 'vintage design dress',
+            price: 75
         },
         {
             id: 6,
-            image: productImage,
+            image: productImage6,
             category: 'Church wear',
-            name: 'Product 6',
-            price: 600
+            name: 'chic layered dress',
+            price: 80
         },
         {
             id: 7,
-            image: productImage,
-            category: 'Church wear',
-            name: 'Product 7',
-            price: 700
+            image: productImage7,
+            category: 'Professional wear',
+            name: 'glamorous office dress',
+            price: 180
         },
         {
             id: 8,
-            image: productImage,
-            category: 'Church wear',
-            name: 'Product 8',
-            price: 800
+            image: productImage8,
+            category: 'Party wear',
+            name: 'lightweight linen dress',
+            price: 100
         },
         {
             id: 9,
-            image: productImage,
+            image: productImage9,
             category: 'Church wear',
-            name: 'Product 9',
-            price: 900
+            name: 'brightly coloured layered dress',
+            price: 99
         },
         {
             id: 10,
-            image: productImage,
-            category: 'Church wear',
-            name: 'Product 10',
-            price: 1000
-        }
+            image: productImage10,
+            category: 'Casual wear',
+            name: 'casual summer dress',
+            price: 65
+        },
+        {
+            id: 11,
+            image: productImage11,
+            category: 'Casual wear',       
+            name: 'bright floral dress',
+            price: 100
+        },
+        {
+            id: 12,
+            image: productImage12,
+            category: 'Formal wear',       
+            name: 'puff sleeve flowy dress',
+            price: 160
+        },
+        {
+            id: 13,
+            image: productImage13,
+            category: 'Casual wear',       
+            name: 'flowy summer dress',
+            price: 110
+        },
+        {
+            id: 14,
+            image: productImage14,
+            category: 'Formal wear',       
+            name: 'modern business dress',
+            price: 90
+        },
+        {
+            id: 15,
+            image: productImage15,
+            category: 'Casual wear',       
+            name: 'flowy floral dress',
+            price: 85
+        },
+        {
+            id: 16,
+            image: productImage16,
+            category: 'Church wear',       
+            name: 'classic buttons and belt dress',
+            price: 110
+        },
+        {
+            id: 17,
+            image: productImage17,
+            category: 'Casual wear',       
+            name: 'simple dress with small straps',
+            price: 70
+        },
+        {
+            id: 18,
+            image: productImage18,
+            category: 'Casual wear',       
+            name: 'minimalist no hands dress',
+            price: 70
+        },
+        {
+            id: 19,
+            image: productImage19,
+            category: 'Church wear',       
+            name: 'vintage pattern dress',
+            price: 95
+        },
+        {
+            id: 20,
+            image: productImage20,
+            category: 'Casual wear',       
+            name: 'simple strappy dress',
+            price: 100
+        },
+        
     ];
     const renderItem = ({ item }) => (
         <View style={productsListStyles.item}>

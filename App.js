4e8b9f1f5c9f9ef38dec3f42bd76c4f28c80818a
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-03 19:51:06 +0000

    Make status bar visible

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 
 import { CartContextProvider } from './contexts/cartContext';
 import DrawerNav from './navigation';
@@ -6,6 +7,7 @@ import DrawerNav from './navigation';
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style = "dark"/>
       <CartContextProvider>
         <DrawerNav/>
       </CartContextProvider>

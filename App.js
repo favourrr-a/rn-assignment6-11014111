6b8832a0f0b14ce0e8c7808b03243ffeb2a34c6e
favourrr-a <fackonu@st.ug.edu.gh>
2024-07-03 02:11:32 +0000

    Wrap cart context provider around entire app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 
+import { CartContextProvider } from './contexts/cartContext';
 import DrawerNav from './navigation';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNav/>
+      <CartContextProvider>
+        <DrawerNav/>
+      </CartContextProvider>
     </NavigationContainer>
   );
 }
